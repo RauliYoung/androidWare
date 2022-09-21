58ad5045bff24f4b4b1bf55b63d6853f10779f9d
RauliYoung <tristan.ellenberg@gmail.com>
2022-09-21 13:23:22 +0300

    new class

--- Tehtava6_1/app/src/main/java/com/example/tehtava6_1/MainActivity.java
+++ Tehtava6_1/app/src/main/java/com/example/tehtava6_1/MainActivity.java
@@ -3,10 +3,17 @@ package com.example.tehtava6_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listViewPresidents = findViewById(R.id.listViewPresidents);
-        listViewPresidents.setAdapter(new ArrayAdapter<President>(
-            this, R.layout.president_layout,
-                GlobalModel.getInstance().getPresidents()
+
+        listViewPresidents.setAdapter(new ArrayAdapter<>(
+                this,
+                R.layout.president_layout,
+                PresidentData.getInstance().getPresidents()
         ));
 
-
+        listViewPresidents.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //log i antaa kohteen listan sijainnin.
+                Log.i("item_click", "clicked " + i);
+            }
+        });
 
     }
+
 }
--- Tehtava6_1/app/src/main/java/com/example/tehtava6_1/President.java
+++ Tehtava6_1/app/src/main/java/com/example/tehtava6_1/President.java
@@ -1,11 +1,20 @@
 package com.example.tehtava6_1;
 
 public class President {
-    private String etunimi;
-    private String sukunimi;
+    private String nimi;
     private int kaudenAlkuVuosi;
     private int kaudenLoppuVuosi;
+    private String lempiNimi;
 
+    public President(String etu, int alku, int loppu,String lempiNimi) {
+        this.nimi = etu;
+        this.kaudenAlkuVuosi = alku;
+        this.kaudenLoppuVuosi = loppu;
+        this.lempiNimi = lempiNimi;
+    }
 
-
+    @Override
+    public String toString() {
+        return this.nimi;
+    }
 }
