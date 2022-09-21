6af5335bfe2a0535522b9d8e50fe0745c8053499
RauliYoung <tristan.ellenberg@gmail.com>
2022-09-21 11:05:16 +0300

    pressajuttuja

--- Tehtava6_1/app/src/main/java/com/example/tehtava6_1/MainActivity.java
+++ Tehtava6_1/app/src/main/java/com/example/tehtava6_1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tehtava6_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listViewPresidents = findViewById(R.id.listViewPresidents);
+        listViewPresidents.setAdapter(new ArrayAdapter<President>(
+            this, R.layout.president_layout,
+                GlobalModel.getInstance().getPresidents()
+        ));
+
+
 
     }
 }
---
+++ Tehtava6_1/app/src/main/java/com/example/tehtava6_1/President.java
@@ -0,0 +1,11 @@
+package com.example.tehtava6_1;
+
+public class President {
+    private String etunimi;
+    private String sukunimi;
+    private int kaudenAlkuVuosi;
+    private int kaudenLoppuVuosi;
+
+
+
+}
